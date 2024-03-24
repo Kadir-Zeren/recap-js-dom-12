b7ba183bd55eee2d1e3805fc995753668c3f6959
kadir-zeren <k4dzeren@gmail.com>
2024-03-24 13:35:27 +0300

    traversinDOM

--- traversingDOM.js
+++ traversingDOM.js
@@ -5,9 +5,33 @@ console.log(addItem.parentElement);
 console.log(addItem.parentNode.parentNode);
 
 const list = document.querySelector(".list");
-
 console.log(list.textContent);
 
 console.log(list.closest("section"));
 
-console.log(list.closest());
+console.log(list.closest("body").querySelector("header h1").innerText);
+
+console.log(addItem.children);
+
+console.log(addItem.children[2]);
+
+console.log(addItem.firstElementChild);
+console.log(addItem.lastElementChild);
+
+console.log(addItem.firstChild);
+
+console.log(document.querySelector("h1").firstChild);
+
+const ul = document.querySelector("ul");
+
+const lists = ul.children;
+console.log(lists);
+
+const js = lists[2];
+console.log(js.innerText);
+
+const css = js.previousElementSibling;
+console.log(css.innerText);
+
+const django = js.nextElementSibling.nextElementSibling;
+console.log(django.innerHTML);
